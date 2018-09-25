63ed92f88d825622ed5be60b68bbcd358b6d302c
tiantang198635 <tiantang198635@126.com>
2018-09-25 12:29:41 +0800

    1.封装全局请求 2.修改登录

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,11 +1,64 @@
-var path="localhost:8080";
-
-/**
- *获取token
- */
-getToken = function(){
-	var token = $.cookie('token');
-	if(token==null||token==''){
-		window.location.href = "/login.html";
+var http = {
+	path:"localhost:8080",
+	ignoreUrl:"auth",//忽略认证的url
+	/**
+	 * 带认证信息的get请求
+	 */
+	authGet:function(url,callback){
+		var token = http.getToken();
+		if(http.validateUrl(url)&&(token==null||token=='')){
+			window.location.href = "/login.html"; 
+			return;
+		}
+		$.ajax({ 
+			type : "GET", 
+			url  : url,   
+			contentType : 'application/json',
+			headers: {
+				Accept: "application/json; charset=utf-8",
+				Authorization: "Bearer " + token
+			}, 
+			success : function(result){
+				callback(result);
+			}
+		});
+	},
+	/**
+	 * 带认证信息的post请求
+	 */
+	authPost:function(url,data,callback){
+		var token = http.getToken();
+		if(http.validateUrl(url)&&(token==null||token=='')){
+			window.location.href = "/login.html"; 
+			return;
+		}
+		$.ajax({ 
+			type : "POST", 
+			url  : url,   
+			contentType : 'application/json',
+			data : JSON.stringify(data), 
+			success : function(result){
+				callback(result);
+			}
+		});
+	},
+	/**
+	 *获取token
+	 */
+	getToken:function(){
+		var token = $.cookie('token');
+		return token;
+	},
+	//判断url是否需要认证，true需要认证，false不需要认证
+	validateUrl:function(url){
+		if(url.indexOf(http.ignoreUrl)!=-1){
+			return false;
+		}else{
+			return true;
+		}
+	},
+	//页面跳转
+	forward:function(url){
+		window.location.href = url;
 	}
 }
--- assets/js/login.js
+++ assets/js/login.js
@@ -3,38 +3,13 @@ $(function () {
          var account = $('#c-account').val();
          var pwd = $('#c-password').val();
          
-		$.ajax({ 
-			type : "POST", 
-			url  : "/sign-user/auth",   
-			contentType : 'application/json',
-			data : JSON.stringify({name:account,pwd:pwd}), 
-			success : function(result){
-				var token = result.token;
-				if(token!=null&&token!=''){
-					getInfo();
-				}
-				/*if(result.code==10000){
-					window.location.href = "/account-index.html";
-				}else{
-					alert(result.result);
-				}*/
-			}
-		});
-
-    });
-	
-	function getInfo(){
-		$.ajax({ 
-			type : "GET", 
-			url  : "/sign-user/user/info",   
-			contentType : 'application/json',
-			headers: {
-				Accept: "application/json; charset=utf-8",
-				Authorization: "Bearer " + getToken()
-			}, 
-			success : function(result){
-				alert(result);
-			}
-		});
-	}
+		 http.authPost("/sign-user/auth",{name:account,pwd:pwd},function(result){ 
+			if(result!=null&&result.code==10000){
+				/*http.authGet("/sign-user/user/info",function(result){
+					alert(result);
+				});*/
+				http.forward("/usercenter.html");
+			} 
+		}); 
+    }); 
 });
